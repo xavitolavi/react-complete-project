91a5df1e634eacaccab07cc789610b41646c885a
xavitolavi <floritxavi@gmail.com>
2021-04-16 17:25:35 +0200

    Extracted ExpenseDate

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
     return (
         <div className="expense-item">
-            <div>{props.date.toDateString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
